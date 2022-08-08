efbf1963a85de36f11a17909972a068db959394a
Alexis Métaireau <alexis@notmyidea.org>
2022-08-08 21:55:43 +0200

    Update

---
+++ build/precache-manifest.3efa322b5c0880100a64c1e53d868195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627b37feca1a044c488202cf62fc00c6",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "71412748ed8e61e3d0f11dcdedfb23d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "16bf90aaa24358ca8527",
+    "url": "/static/css/main.97ad7f85.chunk.css"
+  },
+  {
+    "revision": "16bf90aaa24358ca8527",
+    "url": "/static/js/main.16bf90aa.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
